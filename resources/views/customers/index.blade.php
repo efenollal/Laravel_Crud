b9a7b3f6c043a382e4aa3fedb8386e3a411b6bf5
Edwin Fenollal <efenollal@gmail.com>
2021-01-24 21:59:42 -0500

    Added logic to check if no customers are found in index

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -12,46 +12,54 @@
         </div>
     </div>
    
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
+   
+    @if ($customers->isEmpty())
+        <div class="alert alert-info">
+            <p>No Customers to show</p>
         </div>
-    @endif
-   
-    <table class="table table-bordered">
-        <tr>
-            <td></td>
-            <td>Name</td>
-            <td>Last Name</td>
-            <td>Email</td>
-            <td>Company</td>
-            <td></td>
-        </tr>
+    @else    
         
-        @foreach ($customers as $customer)
-        <tr>
-            <td>{{ ++$i }}</td>
-            <td>{{ $customer->name }}</td>
-            <td>{{ $customer->last_name }}</td>
-            <td>{{ $customer->email }}</td>
-            <td>{{ $customer->company }}</td>
-            <td>
-                <form action="{{ route('customer.destroy',$customer->id) }}" method="POST">
-   
-                    <a class="btn btn-info" href="{{ route('customer.show',$customer->id) }}">Show</a>
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p>{{ $message }}</p>
+            </div>
+        @endif
+        
+        <table class="table table-bordered">
+            <tr>
+                <td></td>
+                <td>Name</td>
+                <td>Last Name</td>
+                <td>Email</td>
+                <td>Company</td>
+                <td></td>
+            </tr>
+            
+            @foreach ($customers as $customer)
+            <tr>
+                <td>{{ ++$i }}</td>
+                <td>{{ $customer->name }}</td>
+                <td>{{ $customer->last_name }}</td>
+                <td>{{ $customer->email }}</td>
+                <td>{{ $customer->company }}</td>
+                <td>
+                    <form action="{{ route('customer.destroy',$customer->id) }}" method="POST">
     
-                    <a class="btn btn-primary" href="{{ route('customer.edit',$customer->id) }}">Edit</a>
-   
-                    @csrf
-                    @method('DELETE')
-      
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-  
-    {!! $customers->links() !!}
-      
+                        <a class="btn btn-info" href="{{ route('customer.show',$customer->id) }}">Show</a>
+        
+                        <a class="btn btn-primary" href="{{ route('customer.edit',$customer->id) }}">Edit</a>
+    
+                        @csrf
+                        @method('DELETE')
+        
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+    
+        {!! $customers->links() !!}
+    
+    @endif
 @endsection
